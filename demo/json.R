836b53fcd5f55f3b2aaa48dfd45337674b7063c3
B. W. Lewis <bwaynelewis@gmail.com>
2011-08-29 11:13:15 -0400

    Updated json demo to be self-contained (includes flotr library)

--- demo/json.R
+++ demo/json.R
@@ -1,14 +1,12 @@
-# This example is intended to run from http://illposed.net/rwebsockjson.html,
-# although the interaction between the scripts and R runs on the user's
-# local machine.
 library('websockets')
 library('RJSONIO')
 
-w = createContext()
+w = createContext(webpage=paste(system.file(package = "websockets"),"rwebsockjson.html", sep = "//"))
 f = function(DATA,WSI,COOKIE)
 {
   n = as.numeric(rawToChar(DATA))
   if(is.na(n)) n = 5
+  cat("n = ",n,"\n")
   n = max(n, 2)
   A = rbind(1:n, runif(n))
   opt = list(lines=list(show='true'),
