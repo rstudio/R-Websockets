154c82baa0a8428033320a4ad86d6ae7400941b9
B. W. Lewis <bwaynelewis@gmail.com>
2011-09-18 20:17:07 -0400

    minimalist socket library

--- src/websockets.h
+++
@@ -1,9 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-
-SEXP setcookie(SEXP COOKIE,SEXP DATA);
-SEXP getcookie(SEXP COOKIE);
-SEXP wsockwrite(SEXP WSI,SEXP DATA);
-SEXP wsockbcast(SEXP DATA);
-SEXP service(SEXP CONTEXT);
-
